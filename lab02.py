173def195259d2f212347c5c2b4335f71aa2b6c7
azizulabedinazmi <azmi00121@gmail.com>
2024-10-28 14:13:37 +0600

    lab02

---
+++ lab02.py
@@ -0,0 +1,11 @@
+# Variable Declaration and Assignment
+name = "John"
+age = 30
+height = 6.1
+is_student = False
+
+# Outputting variable values
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student:", is_student)
